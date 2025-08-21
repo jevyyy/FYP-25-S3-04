dcf76c7e2c6e82a6fbd53d0780f8475ea7466190
Redjoker1412 <kuanxun4@gmail.com>
2025-08-21 17:25:58 +0800

    test

---
+++ Green_Lens/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import CameraScreen from './CameraScreen'; // ← fixed
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <CameraScreen />
+    </GestureHandlerRootView>
+  );
+}
