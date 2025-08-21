3929c59af6cb0a4a0e1e4608de85fae0509d5247
Redjoker1412 <kuanxun4@gmail.com>
2025-08-21 15:40:53 +0800

    update

--- Green_Lens/app.jsx
+++ Green_Lens/app.jsx
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import CameraScreen from './Screenpage/CameraScreen';
+import CameraScreen from './CameraScreen';
 
 export default function App() {
   return (
