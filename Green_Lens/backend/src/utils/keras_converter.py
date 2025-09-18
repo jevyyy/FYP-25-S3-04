57b332fcaf115a7b6f78547ca16f6a28a3d126dc
Terence <tcjy2005@gmail.com>
2025-09-19 04:43:49 +0800

    documentation

--- Green_Lens/backend/src/utils/keras_converter.py
+++ Green_Lens/backend/src/utils/keras_converter.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import tensorflowjs as tfjs
 
+# TO USE UBUNTU 22.04: wsl -d Ubuntu-22.04
+# TO ACTIVATE VENV IN UBUNTU 22.04: source py310env/bin/activate           
 # converts keras mdoel to tfjs model
 # Load your Keras model
 model = tf.keras.models.load_model('flower_img_classifier.keras')
-model = tf.keras.models.load_model('C:\\Users\\User\\Desktop\\Terence\\New folder\\FYP-25-S3-04\\Green_Lens\\backend\\src\\camera\\flower_img_classifier.keras')
 
 # Convert and save as TFJS format
-tfjs.converters.save_keras_model(model, 'C:\\Users\\User\\Desktop\\Terence\\New folder\\FYP-25-S3-04\\Green_Lens\\backend\\src\\camera\\tfjs_model')
+tfjs.converters.save_keras_model(model, 'tfjs_model.keras')
 print("Model converted and saved to ../tfjs_model")
