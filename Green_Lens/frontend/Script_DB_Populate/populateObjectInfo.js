635d3560b608fcae7d539ce3ee669c38d0a05cca
Redjoker1412 <kuanxun4@gmail.com>
2025-11-12 18:06:15 +0800

    update to the script

--- Green_Lens/frontend/Script_DB_Populate/populateObjectInfo.js
+++ Green_Lens/frontend/Script_DB_Populate/populateObjectInfo.js
@@ -906,6 +906,13 @@ const architectureData = [
     description: "A large outdoor performance venue designed for symphonies, concerts, and cultural events.",
     characteristics: "Open-air amphitheatre with tiered seating, elevated stage, and acoustic design to project sound to large audiences.",
     funFact: "Named after philanthropist Shaw Foundation, it is a hub for cultural performances and community gatherings in botanical gardens."
+  },
+  {
+  id: "forest discovery centre",
+  name: "Forest Discovery Centre",
+  description: "An immersive educational hub within the Singapore Botanic Gardens dedicated to rainforest conservation and biodiversity awareness.",
+  characteristics: "Features interactive exhibits, informative displays, and elevated walkways surrounded by lush canopy trees showcasing native forest ecosystems.",
+  funFact: "The centre highlights Singapore’s reforestation efforts and offers a bird’s-eye view of the surrounding treetops from the OCBC Arboretum."
   }
 ];
 
